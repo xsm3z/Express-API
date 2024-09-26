2a1ec7f3fa99899bb7b7c261604bbd5cdb2d2bcf
xsm3z <xsm.zhang@gmail.com>
2024-09-26 19:47:48 -0400

    CRUD functionality

--- controllers/pets.js
+++ controllers/pets.js
@@ -11,4 +11,66 @@ router.post('/', async (req, res) => {
   }
 })
 
+router.get('/', async (req, res) => {
+  try {
+    const foundPets = await Pet.find()
+    res.status(200).json(foundPets)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+router.get('/:petId', async (req, res) => {
+  try {
+    const foundPet = await Pet.findById(req.params.petId);
+    if (!foundPet) {
+      res.status(404);
+      throw new Error('Pet not found.');
+    }
+    res.status(200).json(foundPet);
+  } catch (error) {
+    if (res.statusCode === 404) {
+      res.json({ error: error.message });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
+  }
+});
+
+router.delete('/:petId', async (req, res) => {
+  try {
+    const deletedPet = await Pet.findByIdAndDelete(req.params.petId);
+    if (!deletedPet) {
+      res.status(404);
+      throw new Error('Pet not found.');
+    }
+    res.status(200).json({ message: 'Pet deleted successfully', pet: deletedPet });
+  } catch (error) {
+    if (res.statusCode === 404) {
+      res.json({ error: error.message });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
+  }
+});
+
+router.put('/:petId', async (req, res) => {
+  try {
+    const updatedPet = await Pet.findByIdAndUpdate(req.params.petId, req.body, {
+      new: true
+    })
+    if (!updatedPet) {
+      res.status(404)
+      throw new Error('Pet not found.')
+    }
+    res.status(200).json(updatedPet)
+  } catch (error) {
+    if (res.statusCode === 404) {
+      res.json({ error: error.message })
+    } else {
+      res.status(500).json ({ error: error.message })
+    }
+  }
+})
+
 module.exports = router
